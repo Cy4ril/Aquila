e3ca4950467c85743aca9f4f4321a8979e4f7eb2
Cy4ril <akola996@gmail.com>
2018-05-03 17:31:28 +0100

    hajshaj

---
+++ Assignment.js
@@ -0,0 +1,6 @@
+let NumberOfChildren = 4;
+let Partnername = "Mary";
+let job = "Lawyer";
+let town = "Buea";
+
+console.log("you will be a" +job+ "in" +town+ "and married to" +Partnername+ "and have" +NumberOfChildren+ "kids");
